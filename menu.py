9f1a3588b45c1317528158afb6b2ef0186fe7416
pibe0710 <sd170419@gmail.com>
2025-08-29 17:27:13 -0500

    menu

---
+++ menu.py
@@ -0,0 +1,25 @@
+import math
+
+opcion = 0
+
+while opcion != 4:
+    print("Menú")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+
+    opcion = input("Selecciona una opción (1-5): ")
+
+    if opcion == "1":
+        print("Has escogido la operación: Suma")
+    elif opcion == "2":
+        print("Has escogido la operación: Resta")
+    elif opcion == "3":
+        print("Has escogido la operación: Multiplicacion")
+    elif opcion == "4":
+        print("Has escogido la operación: Division")
+    
+    else:
+        print("Opción no válida. Intenta de nuevo.")
+
